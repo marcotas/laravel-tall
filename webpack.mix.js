e08ed058acd4e99f3d144758b0d7c5064f643daf
Marco Santos <marcotulio.avila@gmail.com>
2022-03-22 19:09:05 -0700

    Customize tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,10 +13,10 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ]);
+    .sass('resources/scss/app.scss', 'public/css/app.css')
+    .options({
+        postCss: [ tailwindcss(require('./tailwind.config.js')) ],
+    });
 
 if (mix.inProduction()) {
     mix.version();
